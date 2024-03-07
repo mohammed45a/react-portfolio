414033dd7ab5ec9cce6d46ad5cea6d6f043b8bf6
Mohammed <maboobakar17@gmail.com>
2024-03-07 20:10:47 +0000

    added a photo of myself as the hero

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="/Users/mohammed/bootcamp/react-portfolio/public/vietnam.jpeg"
           />
         </div>
       </div>
